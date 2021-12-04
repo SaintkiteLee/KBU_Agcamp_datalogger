414deb72750f3057d58cd6cc45babdf69316c51a
SaintkiteLee <saintkite0402@gmail.com>
2021-12-04 16:20:01 +0900

    [UPDATE] 일단 DB-connect 성공

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
-import sqlite3
+import sqlite3, numpy
+inport pandas as pd
+import json
 
 app = Flask(__name__)
 
 
+conn=sqlite3.connect('database.sqlite3', check_same_thread=False)
 
 
 @app.route('/')
@@ -16,7 +19,16 @@ def dashboard():
 
 @app.route('/datalog')
 def datalog():
-    return 'datalog'
+    c = conn.cursor()
+    test_list = []
+    for row in c.execute('SELECT * FROM test_env_log'):
+        print(row)
+        test_list[row] = row
+    for i in test_list :
+        testjson = json.dumps(test_list)
+    print(testjson)
+
+    return testjson
 
 
 
