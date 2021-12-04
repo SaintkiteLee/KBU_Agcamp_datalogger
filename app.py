3f65433fddcb024da50517814ad0e6e0bd1277af
SaintkiteLee <saintkite0402@gmail.com>
2021-12-04 19:40:44 +0900

    [UPDATE] Incoming Data to insert SQLite

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify, make_response
 import sqlite3, numpy
 import pandas as pd
 import json
+import datetime as dt
 from pprint import pprint
 
 app = Flask(__name__)
@@ -37,6 +38,35 @@ def datalog():
 
     return render_template('tables.html', dict=dict)
 
+
+@app.route('/api/in_topic', methods=['POST'])
+def in_topic():
+
+    x = dt.datetime.now()
+
+    if request.method == 'GET':
+        return "GET은 허용되지 않았습니다"
+
+    if request.method == 'POST':
+        response_d = request.get_json()
+        print(response_d)
+        param2 = {
+            'date': x.strftime("%Y.%m.%d"),
+            'time': x.strftime("%H:%M"),
+            'temp_a': response_d['temp'],
+            'temp_b': response_d['temp'],
+            'humidity_a': response_d['humidity'],
+            'humidity_b': response_d['humidity'],
+            'co2': response_d['co2']
+        }
+
+        df = pd.DataFrame.from_records([param2])
+        df_r = df.set_index('date')
+
+        df_r.to_sql('test_env_log', conn, if_exists='append')
+
+
+    return "200"
 
 
 if __name__ == '__main__':
